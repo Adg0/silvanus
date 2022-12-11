8b8fef6111b18e1bcfb6c4b244c6c893e32670c0
3Spear <kalebbirru@gmail.com>
2022-12-11 13:13:30 +0300

    Max supply of H2O set to total water on Earth

--- contracts/WaterToken.sol
+++ contracts/WaterToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
@@ -15,7 +14,8 @@ contract WaterToken is ERC20, Ownable, AccessControl {
     }
 
     function mint(address to, uint256 _volume) public onlyRole(MINTER_ROLE) {
-        //require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        uint256 supply = totalSupply();
+        require(supply + _volume <= (1260 * 10 ** (2 * decimals())), "Max supply of H2O reached.");
         _mint(to, _volume);
     }
 
